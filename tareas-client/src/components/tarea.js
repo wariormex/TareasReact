036d75b9cc0b547c3677ff0770ef569f404c7f6c
wariormex <wariormex@hotmail.com>
2022-04-26 19:42:32 -0500

    Added Date formatting via Moment.js

--- tareas-client/src/components/tarea.js
+++ tareas-client/src/components/tarea.js
@@ -1,6 +1,7 @@
 import React from "react";
 import TareaUpdateForm from "./tareaUpdateForm.js"
 import {useEffect, userEffect, useState} from "react";
+import Moment from "moment";
 
 const Tarea = ({tarea, onDeleteFn, onUpdateFn, formState, formStateFn}) => {
     const [showForm, setShowForm] = useState(formState);
@@ -13,6 +14,15 @@ const Tarea = ({tarea, onDeleteFn, onUpdateFn, formState, formStateFn}) => {
     const  clickUpdateTarea = () =>{
         setShowForm(!showForm)
     }
+
+    function formatDate(date) {
+        const currentMonth = date.getMonth();
+        const monthString = currentMonth >= 10 ? currentMonth : `0${currentMonth}`;
+        const currentDate = date.getDate();
+        const dateString = currentDate >= 10 ? currentDate : `0${currentDate}`;
+        return `${date.getFullYear()}-${monthString}-${currentDate}`;
+    }
+
     return(
         <div>
             <h5>{tarea.name}</h5>
@@ -20,13 +30,15 @@ const Tarea = ({tarea, onDeleteFn, onUpdateFn, formState, formStateFn}) => {
             <h5>{`Id: ${tarea.id}`}</h5>
             <h5>{`Materia: ${tarea.materia}`}</h5>
             <h5>{`Puntos: ${tarea.puntos}`}</h5>
-            <h5>{`Fecha de Entrega: ${tarea.fechaEntrega}`}</h5>
-            <h5>{`Fecha de Creacion: ${tarea.fechaCreacion}`}</h5>
+            <h5>{`Fecha de Entrega: ${Moment(tarea.fechaEntrega).format("DD-MM-YYYY hh:mm:ss a")}`}</h5>
+            <h5>{`Fecha de Creacion: ${Moment(tarea.fechaCreacion).format("DD-MM-YYYY hh:mm:ss a")}`}</h5>
             <button className="new-btn" onClick={clickDeleteTarea}>{"Borrar Tarea"}</button>
             <button className="new-btn" onClick={clickUpdateTarea}>{showForm ? "Cerrar" : "Actualizar Tarea"}</button>
             {showForm && <TareaUpdateForm onClickFn={onUpdateFn} tareaToUpdate = {tarea} closeFormFn = {clickUpdateTarea}></TareaUpdateForm>}
         </div>
     );
+
+    
     
 }
 
